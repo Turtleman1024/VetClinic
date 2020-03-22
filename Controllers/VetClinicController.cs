b8c142f95512851f9f22791f4b08ffd3273ad2c5
Turtleman1024 <kmurry6262@gmail.com>
2020-03-22 13:12:00 -0700

    Add project files.

---
+++ Controllers/VetClinicController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VetClinic.Controllers
+{
+    public class VetClinicController : Controller
+    {
+        [HttpGet("api/name")]
+        public IActionResult Get()
+        {
+            return Ok(new { name = "Turtleman's Vet Clinic" });
+        }
+    }
+}
